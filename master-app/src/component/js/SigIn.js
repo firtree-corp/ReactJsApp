a61a5d0ee7fe8a6e092190110fa79290deeba507
RIGAL <dorian.rigal@epitech.eu>
2019-03-01 16:08:30 +0100

    [EDIT] SignUp and SignIn component

--- master-app/src/component/js/SigIn.js
+++ master-app/src/component/js/SigIn.js
@@ -5,21 +5,36 @@ class SignInInput extends Component {
     render() {
         return (
             <div className="inputDiv">
-                <p className="text">{this.props.name}</p>
-                <input className="input"/>
+                <p className="text">{this.props.name} :</p>
+                <input className="input" value={this.props.value} type={this.props.type} onChange={(value) => {this.props.func(value)}}/>
             </div>
         );
     }
 }
 
 export default class SignIn extends Component {
+    constructor() {
+        super()
+        this.state = {ident: "", pwd: ""}
+        this.handleIdent = this.handleIdent.bind(this)
+        this.handlePwd = this.handlePwd.bind(this)
+    }
+
+    handleIdent(ident) {
+        this.setState({ident: ident.target.value})
+    }
+
+    handlePwd(pwd) {
+        this.setState({pwd: pwd.target.value})
+    }
+
     render() {
         return (
             <div className="mainBlock">
                 <p className="textTitle">Connexion</p>
                 <div className="inputBlock">
-                    <SignInInput name="Email ou Nom de compte"/>
-                    <SignInInput name="Mot de Passe"/>
+                    <SignInInput name="Email ou Nom de compte" type="text" func={this.handleIdent} value={this.state.ident}/>
+                    <SignInInput name="Mot de Passe" type="password" func={this.handlePwd} value={this.state.pwd}/>
                 </div>
                 <button className="button">Valider</button>
             </div>
--- master-app/src/component/js/SignUp.js
+++ master-app/src/component/js/SignUp.js
@@ -5,22 +5,42 @@ class SignUpInput extends Component {
     render() {
         return (
             <div className="inputDiv">
-                <p className="text">{this.props.name}</p>
-                <input className="input"/>
+                <p className="text">{this.props.name} :</p>
+                <input className="input" value={this.props.value} type={this.props.type} onChange={(value) => {this.props.func(value)}}/>
             </div>
         );
     }
 }
 
 export default class SignUp extends Component {
+    constructor() {
+        super()
+        this.state = {user: "", mail: "", pwd: ""}
+        this.handleUser = this.handleUser.bind(this)
+        this.handleMail = this.handleMail.bind(this)
+        this.handlePwd = this.handlePwd.bind(this)
+    }
+
+    handleUser(user) {
+        this.setState({user: user.target.value})
+    }
+
+    handleMail(mail) {
+        this.setState({mail: mail.target.value})
+    }
+
+    handlePwd(pwd) {
+        this.setState({pwd: pwd.target.value})
+    }
+
     render() {
         return (
             <div className="mainBlock">
                 <p className="textTitle">Inscription</p>
                 <div className="inputBlock">
-                    <SignUpInput name="Nom de compte"/>
-                    <SignUpInput name="Email"/>
-                    <SignUpInput name="Mot de Passe"/>
+                    <SignUpInput name="Nom de compte" type="text" func={this.handleUser} value={this.state.user}/>
+                    <SignUpInput name="Email" type="email" func={this.handleMail} value={this.state.mail}/>
+                    <SignUpInput name="Mot de Passe" type="password" func={this.handlePwd} value={this.state.pwd}/>
                 </div>
                 <button className="button">Valider</button>
             </div>
